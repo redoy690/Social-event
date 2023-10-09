9aeea7608b5fdc45057b7153b6a4a886dfccb5b4
redoy690 <redoyislam995@gmail.com>
2023-10-09 08:22:27 +0600

    Complete login and registration page with firebase authentication

--- src/pages/Home/OurClint.jsx
+++ src/pages/Home/OurClint.jsx
@@ -4,7 +4,9 @@ const OurClint = () => {
     return (
         <div>
             <h4 className="text-center text-4xl text-[#FFBB00] rounded-xl font-bold my-6 bg-slate-400 border border-[#FF6F33] py-6">Our Client List</h4>
-            
+            <div>
+                
+            </div>
         </div>
     );
 };
